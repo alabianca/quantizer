c6cac34a32a9eb3fa38f3e766a820e56cd66546f
alabianca <labianca.alex@gmail.com>
2025-01-03 19:42:56 -0800

    more readme examples

--- quantizer.go
+++ quantizer.go
@@ -11,6 +11,9 @@ type Quantizer interface {
 	Quantize(src image.Image, colors color.Palette) (image.Image, error)
 }
 
+// Quantize uses the default image quantization algorithm "median cut"
+// to quantize src using the provided colors palette. The quantized image
+// will be returned and will have the same dimensions as the provided src image
 func Quantize(src image.Image, colors color.Palette) (image.Image, error) {
 	im, err := mediancut.Quantize(src, colors, mediancut.QuickSelect)
 	return im, err
